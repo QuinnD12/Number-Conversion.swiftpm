66b29a4afceac9ca45c93d2b247ffdc8d006dfd9
Bryan Landsnes <blandsnes6236@stu.d214.org>
2023-09-20 10:57:27 -0500

    I'm done

--- BinaryToDecimalView.swift
+++ BinaryToDecimalView.swift
@@ -1,12 +1,41 @@
 import SwiftUI
 
 struct BinaryToDecimal: View {
+    @State var text = ""
+    @State var finalAnswer = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Enter Decimal Below")
+                .padding(.bottom)
+            TextField("Placeholder", text: $text)
+                .frame(width: 90, height: .infinity, alignment: .center)
+                .padding(.bottom)
+            Button("PRESS ME") {
+                ButtonPress()
+            }
+            Text(finalAnswer)
+             
+        
+            
         }
+    }
+    
+    func ButtonPress() {
+        
+        var decimalValue = Int(text)!
+        var binaryNumber = ""
+        while(decimalValue > 0) {
+            if ((decimalValue & 1) == 1){
+                binaryNumber += "1"
+            }
+            else {
+                binaryNumber += "0"
+            }
+            
+            decimalValue >>= 1
+        }
+                
+        finalAnswer = "Your number is: \(String(binaryNumber.reversed()))"
+        
     }
 }
